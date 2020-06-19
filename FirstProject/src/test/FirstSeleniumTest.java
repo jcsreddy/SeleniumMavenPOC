eda0eafb759c49136ef956e6e86b7e172905928b
CHANDRASEKHARJANGA <CHANDRASEKHARJANGA@DESKTOP-EFCAI7I>
2020-06-19 21:47:26 +0530

    Push first selenium program to git

---
+++ FirstProject/src/test/FirstSeleniumTest.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstSeleniumTest {
+	
+	public static void main(String[] args) {
+		
+		//WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\CHANDRASEKHARJANGA\\git\\SeleniumPOC\\FirstProject\\lib\\drivers\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "\\FirstProject\\lib\\drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev");
+		
+		driver.quit();
+	}
+
+}
