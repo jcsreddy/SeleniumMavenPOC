43b51e8056b00ed2a500577f41243f716a4112a2
CHANDRASEKHARJANGA <CHANDRASEKHARJANGA@DESKTOP-EFCAI7I>
2020-06-20 14:47:35 +0530

    Push changes to git with webdrivermanager

--- FirstProject/src/test/FirstSeleniumTest.java
+++ FirstProject/src/test/FirstSeleniumTest.java
@@ -9,7 +9,7 @@ public class FirstSeleniumTest {
 	public static void main(String[] args) {
 		
 		//WebDriver driver = new FirefoxDriver();
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\CHANDRASEKHARJANGA\\git\\SeleniumPOC\\FirstProject\\lib\\drivers\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\CHANDRASEKHARJANGA\\git\\SeleniumPOC\\FirstProject\\lib\\drivers\\chromedriver.exe");
 		//System.setProperty("webdriver.chrome.driver", "\\FirstProject\\lib\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.selenium.dev");
--- SeleniumMavenFrameworkWithJava/src/test/java/BrowserTest.java
+++ SeleniumMavenFrameworkWithJava/src/test/java/BrowserTest.java
@@ -1,6 +1,10 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserTest {
 	
@@ -9,14 +13,27 @@ public class BrowserTest {
 		String projectPath = System.getProperty("user.dir");
 		System.out.println("The project path is "+projectPath);
 		
-		System.setProperty("webdriver.chrome.driver", projectPath+"\\drivers\\chromedriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		System.setProperty("webdriver.gecko.driver", projectPath+"\\drivers\\geckodriver\\geckodriver.exe");
-		//WebDriver driver = new FirefoxDriver();
+		//System.setProperty("webdriver.chrome.driver", projectPath+"\\drivers\\chromedriver\\chromedriver.exe");
+		//WebDriverManager.chromedriver().setup();
+		//WebDriver driver = new ChromeDriver();
+		
+		//System.setProperty("webdriver.gecko.driver", projectPath+"\\drivers\\geckodriver\\geckodriver.exe");
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		
 		driver.get("https://www.selenium.dev/");
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		String str = driver.getCurrentUrl();
-		System.out.println(str);
-		driver.quit();
+		System.out.println("The current URL is :"+str);
+		
+		//driver.quit();
 		
 	}
 
